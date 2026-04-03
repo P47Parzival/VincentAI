166b8c182e03260d896147e44ceb52cd052eadba
siddhant4357 <siddhantsankesara@gmail.com>
2026-04-03 13:30:28 +0530

    added videos in landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
 import Dashboard from './pages/Dashboard'
 
 const App = () => {
-  return <Dashboard />
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
 }
 
 export default App
